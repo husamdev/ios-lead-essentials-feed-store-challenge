58b069d45309de4e22dabb29c226ca7d97207a9d
Husam <husam.dayya@gotocme.com>
2021-03-17 08:16:02 +0200

    Added `ManagedCache` and `ManagedFeedImage` core data entities

---
+++ FeedStoreChallenge/CoreDataModels/ManagedCache.swift
@@ -0,0 +1,17 @@
+//
+//  ManagedCache.swift
+//  FeedStoreChallenge
+//
+//  Created by Husam Dayya on 17/03/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedCache)
+public class ManagedCache: NSManagedObject {
+    @NSManaged public var timestamp: Date
+    @NSManaged public var feed: NSOrderedSet
+}
---
+++ FeedStoreChallenge/CoreDataModels/ManagedFeedImage.swift
@@ -0,0 +1,20 @@
+//
+//  ManagedFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Husam Dayya on 17/03/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedFeedImage)
+public class ManagedFeedImage: NSManagedObject {
+	@NSManaged public var id: String
+	@NSManaged public var imageDescription: String?
+	@NSManaged public var location: String?
+	@NSManaged public var url: URL
+	@NSManaged public var cache: ManagedCache
+}
